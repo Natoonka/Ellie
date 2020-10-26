a1aa731e4bbfeac96817c56cc2550d0823c66d62
Natoonka <natalkakostecka@gmail.com>
2020-10-26 14:44:00 +0100

    Added options menu functionality.

---
+++ Ellie/Assets/Scripts/Dialogue/StartDialogue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StartDialogue
+{
+
+    public string[] names;
+
+    [TextArea(2, 4)]
+    public string[] sentences;
+
+}
---
+++ Ellie/Assets/Scripts/Dialogue/StartScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StartScript : MonoBehaviour
+{
+    [SerializeField] public TextMeshProUGUI nameText;
+    [SerializeField] public TextMeshProUGUI dialogueText;
+
+    public Animator animatorD;
+
+    public StartDialogue dialogue;
+
+    private Queue<string> names;
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        names = new Queue<string>();
+        sentences = new Queue<string>();
+        StartDialogue(dialogue);
+    }
+
+    public void StartDialogue(StartDialogue dialogue)
+    {
+        animatorD.SetBool("IsOpen", true);
+
+        names.Clear();
+        sentences.Clear();
+
+        foreach (string name in dialogue.names)
+        {
+            names.Enqueue(name);
+        }
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        
+        DisplayNextSentence();
+
+    }
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string name = names.Dequeue();
+        string sentence = sentences.Dequeue();
+
+        nameText.text = name;
+        dialogueText.text = sentence;
+    }
+    public void EndDialogue()
+    {
+        animatorD.SetBool("IsOpen", false);
+        Application.LoadLevel("PokojEllie");
+    }
+
+}
